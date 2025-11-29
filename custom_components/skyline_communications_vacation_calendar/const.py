705ea43820d9c7a4905b3617d02b3464df7aa3fb
Robbe-B <103053873+Robbe-B@users.noreply.github.com>
2025-11-29 13:52:43 +0100

    Transition from a device to a service

--- custom_components/skyline_communications_vacation_calendar/const.py
+++ custom_components/skyline_communications_vacation_calendar/const.py
@@ -2,6 +2,10 @@
 
 DOMAIN = "skyline_communications_vacation_calendar"
 DOMAIN_METRICS_URL = "https://domainmetrics-skyline.on.dataminer.services"
+NAME = "Vacation Calendar"
+SERVICE_NAME = f"SLC {NAME}"
+MANUFACTURER_NAME = "Employees of Skyline Communications"
+MODEL_NAME = f"Domain Metrics - {NAME}"
 CONF_FULLNAME = "full_name"
 CONF_ELEMENT_ID = "element_id"
 DEFAULT_SCAN_INTERVAL = 3600
